2996b28e3b0afd92945c25ca9d278e60aca6100c
phuongtdc <phuongtdc@DESKTOP-UNP4QHS>
2020-04-01 23:53:23 +0700

    add copyright

--- components/MapInteractive.vue
+++ components/MapInteractive.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-    <l-map :center="[16.109,102.797]" :zoom="6" style="height: 500px;" :options="mapOptions">
-      <l-choropleth-layer :data="pyDepartmentsData" titleKey="name" idKey="id" :value="value" geojsonIdKey="id" :geojson="paraguay" 
+    <l-map :center="[16.109,102.797]" :zoom="6" style="height: 500px;">
+      <l-choropleth-layer :data="pyDepartmentsData" titleKey="name" idKey="id" :value="value" geojsonIdKey="id" :geojson="paraguay"
         :colorScale="colorScale" 
         :strokeColor="strokeColor" 
         :currentStrokeColor="currentStrokeColor"
         :strokeWidth="strokeWidth"
-        :currentStrokeWidth="currentStrokeWidth">
+        :currentStrokeWidth="currentStrokeWidth"
+        @click="openPopUp(circle.center, 'circle')">
         <template slot-scope="props">
           <l-info-control :item="props.currentItem" :unit="props.unit" title="Số ca nhiễm"/>
-          
         </template>
       </l-choropleth-layer>
-      <l-tile-layer :url="url"></l-tile-layer>
+      <l-tile-layer :url="url" :attribution="tileOptions.attribution" :noWrap="true"></l-tile-layer>
+      <l-circle
+      :lat-lng="circle.center"
+      :radius="circle.radius"
+      :color="circle.color"
+      @click="openPopUp(circle.center, 'circle')"
+      />
     </l-map>
   </div>
 </template>
@@ -23,18 +29,20 @@ import { InfoControl, ReferenceChart, ChoroplethLayer } from 'vue-choropleth'
 import geojson  from '../assets/data/map_vn.json'
 import paraguay from '../assets/data/map_vn.json'
 import pyDepartmentsData from '../assets/data/provinces.json'
-import {LMap, LTileLayer} from 'vue2-leaflet';
+import {LMap, LTileLayer, LPopup, LCircle} from 'vue2-leaflet';
 
 export default {
   name: "app",
   components: { 
     LMap,
     LTileLayer,
+    LPopup, LCircle,
     'l-info-control': InfoControl, 
     'l-reference-chart': ReferenceChart, 
     'l-choropleth-layer': ChoroplethLayer 
   },
   data() {
+
     return {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       pyDepartmentsData,
@@ -44,12 +52,27 @@ export default {
         key: "case",
         metric: "Số người bị"
       },
+      tileOptions: {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      },
       strokeColor: "fff",
       currentStrokeColor: 'cc0909',
       strokeWidth: 0.5, 
-      currentStrokeWidth : 0.8
+      currentStrokeWidth : 0.8,
+      circle: {
+        center: [10.667,106.875],
+        radius: 4500,
+        color: 'red'
+      }
+    }
+  },
+
+  methods: {
+    openPopUp (latLng, caller) {
+      console.log('hi');
     }
   }
+
 }
 </script>
 <style>
