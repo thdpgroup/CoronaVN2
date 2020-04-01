47ccd73fa2b7d570ffbc0281dc54c6c7c2896f8d
phuongtdc <phuongtdc@DESKTOP-UNP4QHS>
2020-04-01 18:22:39 +0700

    add vnGeo generation

---
+++ vnGeoJson/vnGeoJson/Form1.cs
@@ -0,0 +1,89 @@
+﻿using GeoJSON.Net.Converters;
+using GeoJSON.Net.Feature;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vnGeoJson
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      string dataJson = File.ReadAllText("./data/map_vn.geo.json");
+      var data2 = JsonConvert.DeserializeObject<GeoVn>(dataJson);
+      int i = 0;
+
+      foreach (var feature in data2.features)
+      {
+        i++;
+        feature.properties.Id = i;
+      }
+
+      //---- convert to multi polygon
+      GeoVnPolygon geoMultiPolygon = new GeoVnPolygon()
+      {
+        type = data2.type,
+        features = new List<FeatureMultigon>()
+    };
+
+      foreach (var feature in data2.features)
+      {
+        geoMultiPolygon.features.Add(new FeatureMultigon() {
+          type = feature.type,
+          properties = feature.properties,
+          geometry = new GeometryMulti(feature.geometry.coordinates)
+        });
+      }
+
+      var dataSerialize = JsonConvert.SerializeObject(geoMultiPolygon);
+      CreateJsonFile(dataSerialize, "geo_vn.json");
+    }
+
+    private static void CreateJsonFile(string dataSerialize,string fileName)
+    {
+      if (File.Exists(fileName))
+      {
+        File.Delete(fileName);
+      }
+
+      File.WriteAllText(fileName, dataSerialize);
+    }
+
+    private void btnProvince_Click(object sender, EventArgs e)
+    {
+      var provinces = new List<ProvinceJson>();
+
+      string dataJson = File.ReadAllText("./data/map_vn.geo.json");
+      var data2 = JsonConvert.DeserializeObject<GeoVn>(dataJson);
+      int i = 0;
+
+      foreach (var feature in data2.features)
+      {
+        i++;
+        provinces.Add(new ProvinceJson() {
+          Id = i,
+          Case = 0,
+          color = 0,
+          name = feature.properties.province
+        });
+      }
+
+      var dataSerialize = JsonConvert.SerializeObject(provinces);
+      CreateJsonFile(dataSerialize, "provinces.json");
+    }
+  }
+}
---
+++ vnGeoJson/vnGeoJson/geovn.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vnGeoJson
+{
+  public class Geometry2
+  {
+    public string type { get; set; }
+    public List<List<List<double>>> coordinates { get; set; }
+  }
+
+  public class Geometry
+  {
+    public string type { get; set; }
+    public List<List<List<double>>> coordinates { get; set; }
+    public List<Geometry2> geometries { get; set; }
+  }
+
+  public class GeometryMulti
+  {
+    public GeometryMulti()
+    {
+      type = "MultiPolygon";
+    }
+
+    public GeometryMulti(List<List<List<double>>> data)
+    {
+      coordinates = new List<List<List<List<double>>>>();
+      type = "MultiPolygon";
+      coordinates.Add(data);
+    }
+
+    public string type { get; set; }
+    public List<List<List<List<double>>>> coordinates { get; set; }
+    public List<Geometry2> geometries { get; set; }
+  }
+
+  public class Feature
+  {
+    public string type { get; set; }
+    public Geometry geometry { get; set; }
+    public geovn.Properties properties { get; set; }
+  }
+
+  public class FeatureMultigon
+  {
+    public string type { get; set; }
+    public GeometryMulti geometry { get; set; }
+    public geovn.Properties properties { get; set; }
+  }
+
+  public class GeoVn
+  {
+    public string type { get; set; }
+    public List<Feature> features { get; set; }
+  }
+
+
+  public class GeoVnPolygon
+  {
+    public string type { get; set; }
+    public List<FeatureMultigon> features { get; set; }
+  }
+}
+
+namespace geovn
+{
+  public class Properties
+  {
+    public string country { get; set; }
+    public string province { get; set; }
+    public int Id { get; set; }
+
+    public int Case { get; set; }
+
+    public int color { get; set; }
+  }
+}
