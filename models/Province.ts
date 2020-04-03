c6ca795749be5e6898b79290b95835b6d443bebc
phuongtdc <phuongtdc@DESKTOP-UNP4QHS>
2020-04-03 23:14:24 +0700

    refactor patients/Timelines

---
+++ models/Province.ts
@@ -0,0 +1,9 @@
+interface IProvince { 
+    id:         Number,
+    name:       string, 
+    case:       Number,
+    recovered:  Number,
+    death:      Number,
+    new:        Number,
+    color:      string
+} 
