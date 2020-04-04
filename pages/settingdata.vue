0c65a8aa822fd55e6db035e4e83ea697f82d8014
NGUYEN Le Quang Hung <hnguyen13@amaris.com>
2020-04-04 18:17:59 +0700

    - add setting page

---
+++ pages/settingdata.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <tableData> </tableData>
+  </div>
+</template>
+
+<script>
+
+import tableData from '~/components/tableData.vue'
+
+export default {
+  components: {
+    tableData
+  }
+}
+</script>
