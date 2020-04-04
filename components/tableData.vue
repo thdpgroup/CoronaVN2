d18cc3e2bfb25c83aae59dfc5652db6ecb69188b
NGUYEN Le Quang Hung <hnguyen13@amaris.com>
2020-04-05 00:01:17 +0700

    - update

--- components/tableData.vue
+++ components/tableData.vue
@@ -1,23 +1,90 @@
 <template>
-  <div id="app" class="container background-color">
-    <div style="padding-bottom:10px">
-      Total : {{ totalCase }} - Recovered : {{ totalRecovered }} - totalDeath :
-      {{ death }}
+  <v-app>
+    <div id="app" class="container background-color">
+      <div style="padding-bottom:10px">
+        Total : {{ totalCase }} - Recovered : {{ totalRecovered }} - death :
+        {{ totalDeath }}
+      </div>
+      <div>
+        <v-data-table
+          class="elevation-1"
+          :items="Details"
+          :headers="DetailHeader"
+          sort-by="case"
+          sort-desc="true"
+          items-per-page="-1"
+        >
+          <template v-slot:top>
+            <v-toolbar flat color="white">
+              <v-dialog v-model="dialog" max-width="500px">
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">{{ editedItem.name }}</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.name"
+                            label="name"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.case"
+                            label="case"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.recovered"
+                            label="recovered"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.death"
+                            label="death"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.new"
+                            label="new"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.color"
+                            label="color"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="close"
+                      >Cancel</v-btn
+                    >
+                    <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.actions="{ item }">
+            <v-icon small class="mr-2" @click="editItem(item)">
+              mdi-pencil
+            </v-icon>
+          </template>
+        </v-data-table>
+      </div>
     </div>
-    <div>
-      <v-data-table
-        class="elevation-1"
-        :items="Details"
-        :headers="DetailHeader"
-        item-key="ReInvoicingId"
-        sort-by="case"
-        sort-desc="true"
-        multi-sort="true"
-        items-per-page="-1"
-      >
-      </v-data-table>
-    </div>
-  </div>
+  </v-app>
 </template>
 
 <script>
@@ -30,6 +97,7 @@ export default {
       totalCase: 239,
       totalRecovered: 90,
       totalDeath: 0,
+      dialog: false,
       DetailHeader: [
         {
           text: 'id',
@@ -62,12 +130,63 @@ export default {
         {
           text: 'markerLocation',
           value: 'markerLocation'
-        }
-      ]
+        },
+        { text: 'Actions', value: 'actions' }
+      ],
+      editedIndex: -1,
+      editedItem: {
+        id: '',
+        name: '',
+        case: '',
+        recovered: '',
+        death: '',
+        new: '',
+        color: '',
+        markerLocation: []
+      },
+      defaultItem: {
+        id: '',
+        name: '',
+        case: '',
+        recovered: '',
+        death: '',
+        new: '',
+        color: '',
+        markerLocation: []
+      }
+    }
+  },
+  watch: {
+    dialog(val) {
+      val || this.close()
     }
   },
   mounted() {
     this.Details = provinces
+  },
+  methods: {
+    editItem(item) {
+      this.dialog = false
+      this.editedIndex = this.Details.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+    close() {
+      this.dialog = false
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      }, 300)
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.Details[this.editedIndex], this.editedItem)
+      } else {
+        this.Details.push(this.editedItem)
+      }
+      this.close()
+    }
   }
 }
 </script>
