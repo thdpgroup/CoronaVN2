524ded6593238e22c386e07f33a21da5624908eb
anhdai <lanhdai1988@gmail.com>
2020-04-12 00:48:17 +0700

    fix header axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.BASE_URL || 'http://ec2-174-129-61-159.compute-1.amazonaws.com:3000'
+  baseURL: process.env.BASE_URL || 'http://ec2-174-129-61-159.compute-1.amazonaws.com:3000',
+  headers: {'Access-Control-Allow-Origin': '*'}
 })
