b34d751b3a2b7f68677a7ef96cd204af2dfe7dae
anhdai <lanhdai1988@gmail.com>
2020-04-20 01:41:01 +0700

    feat(search): add search feature

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.BASE_URL || 'http://ec2-174-129-61-159.compute-1.amazonaws.com:3000',
-  headers: {'Access-Control-Allow-Origin': '*'}
+  baseURL: process.env.BASE_URL || 'http://ec2-174-129-61-159.compute-1.amazonaws.com:3000'
 })
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const consola = require('consola')
+const cors = require('cors')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 
